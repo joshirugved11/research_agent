72cf890ffb15359f40e47f958d41d0c732d83257
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-04-29 11:50:19 +0530

    summarising the papers using existing models

---
+++ models/summariser.py
@@ -0,0 +1,11 @@
+# summarizer_model.py
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+def load_summarizer(model_name="facebook/bart-large-cnn"):
+    """Load a Hugging Face summarization model pipeline."""
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
+    return summarizer

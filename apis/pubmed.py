4af7322bc480cdacc1f6aff58d17c4aeefabfc0b
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-06-17 13:22:07 +0530

    modified imports

--- apis/pubmed.py
+++ apis/pubmed.py
@@ -1,16 +1,16 @@
-from Bio import entrez
+from Bio import Entrez
 
-entrez.email = "rugvedsamruddhi@gmail.com"
+Entrez.email = "rugvedsamruddhi@gmail.com"
 
 def fetch_papers_from_pubmed(query: str, max_results: int = 10) -> list:
-    handle = entrez.esearch(db="pubmed", term=query, retmax=max_results)
-    record = entrez.read(handle)
+    handle = Entrez.esearch(db="pubmed", term=query, retmax=max_results)
+    record = Entrez.read(handle)
     ids = record["IdList"]
 
     papers = []
 
     for pubmed_id in ids:
-        fetch_handle = entrez.efetch(db="pubmed", id=pubmed_id, retmode="text")
+        fetch_handle = Entrez.efetch(db="pubmed", id=pubmed_id, retmode="text")
         abstract = fetch_handle.read()
         papers.append({
             "title": f"PubMed Article {pubmed_id}",

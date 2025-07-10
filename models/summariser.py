92759abab011da36123d77eab42487756a06c7b5
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-07-10 15:11:05 +0530

    changed facebook bart model to distilbart-cnn model

--- models/summariser.py
+++ models/summariser.py
@@ -2,7 +2,7 @@
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
-def load_summarizer(model_name="facebook/bart-large-cnn"):
+def load_summarizer(model_name="sshleifer/distilbart-cnn-12-6"):
     """Load a Hugging Face summarization model pipeline."""
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
